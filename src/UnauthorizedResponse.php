69d0118228faf1dd924a82eb6801d0e2d79cdacb
Joan Fabrégat <joan@codeinc.fr>
2018-04-13 18:37:19 +0200

    adds Unauthorized

---
+++ src/UnauthorizedResponse.php
@@ -0,0 +1,47 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     23/02/2018
+// Time:     19:22
+// Project:  Psr7Responses
+//
+declare(strict_types = 1);
+namespace CodeInc\Psr7Responses;
+
+/**
+ * Class UnauthorizedResponse
+ *
+ * @package CodeInc\Psr7Responses
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+class UnauthorizedResponse extends HtmlResponse {
+    /**
+     * UnauthorizedResponse constructor.
+     *
+     * @param string|null $html
+     * @param null|string $charset
+     * @param array $headers
+     * @param null $body
+     * @param string $version
+     * @param null|string $reason
+     */
+	public function __construct(string $html = null, ?string $charset = null, array $headers = [],
+		$body = null, string $version = '1.1', ?string $reason = null)
+	{
+		parent::__construct($html ?? '', $charset, 401, $headers, $version, $reason);
+	}
+}
