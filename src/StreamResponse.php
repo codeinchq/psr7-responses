55d4be78340e4db9cbd0a0b0301f6eb728620e29
Joan Fabrégat <joan@codeinc.fr>
2018-07-25 10:57:24 +0200

    new methods getStream(), getCharset(), getContentLength(), getFileName() and isAsAttachment()

--- src/StreamResponse.php
+++ src/StreamResponse.php
@@ -35,11 +35,36 @@ use function GuzzleHttp\Psr7\stream_for;
  */
 class StreamResponse extends Response
 {
+    /**
+     * @var \GuzzleHttp\Psr7\Stream
+     */
+    private $stream;
+
+    /**
+     * @var null|string
+     */
+    private $charset;
+
+    /**
+     * @var int|null
+     */
+    private $contentLength;
+
+    /**
+     * @var null|string
+     */
+    private $fileName;
+
+    /**
+     * @var bool
+     */
+    private $asAttachment;
+
 	/**
 	 * StreamResponse constructor.
 	 *
 	 * @param $resource
-	 * @param null|string $mimeType
+	 * @param null|string $charset
 	 * @param int|null $contentLength
 	 * @param null|string $fileName
 	 * @param bool $asAttachment
@@ -48,24 +73,78 @@ class StreamResponse extends Response
 	 * @param string $version
 	 * @param null|string $reason
 	 */
-	public function __construct($resource, ?string $mimeType = null, ?int $contentLength = null,
+	public function __construct($resource, ?string $charset = null, ?int $contentLength = null,
 		?string $fileName = null, bool $asAttachment = false, int $status = 200, array $headers = [],
 		string $version = '1.1', ?string $reason = null)
 	{
-		$stream = stream_for($resource);
+		$this->stream = stream_for($resource);
+        $this->charset = $charset;
+        $this->contentLength = $contentLength;
+        $this->fileName = $fileName;
+        $this->asAttachment = $asAttachment;
 
 		// adding headers
-		if ($mimeType) {
-			$headers["Content-Type"] = $mimeType;
+		if ($charset) {
+			$headers["Content-Type"] = $charset;
 		}
 		$headers["Content-Disposition"] = $asAttachment ? "attachment" : "inline";
 		if ($fileName) {
 			$headers["Content-Disposition"] .= sprintf("; filename=\"%s\"", $fileName);
 		}
-		if ($contentLength !== null || ($contentLength = $stream->getSize()) !== null) {
+		if ($contentLength !== null || ($contentLength = $this->stream->getSize()) !== null) {
 			$headers["Content-Length"] = $contentLength;
 		}
 
-		parent::__construct($status, $headers, $stream, $version, $reason);
+		parent::__construct($status, $headers, $this->stream, $version, $reason);
 	}
+
+    /**
+     * Returns the stream.
+     *
+     * @return \GuzzleHttp\Psr7\Stream
+     */
+    public function getStream():\GuzzleHttp\Psr7\Stream
+    {
+        return $this->stream;
+    }
+
+    /**
+     * Returns the mime type if set or null.
+     *
+     * @return null|string
+     */
+    public function getCharset():?string
+    {
+        return $this->charset;
+    }
+
+    /**
+     * Returns the content length if set or null.
+     *
+     * @return int|null
+     */
+    public function getContentLength():?int
+    {
+        return $this->contentLength;
+    }
+
+    /**
+     * Returns the file name if set or null.
+     *
+     * @return null|string
+     */
+    public function getFileName():?string
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * Verify if the response must be downloaded.
+     *
+     * @return bool
+     */
+    public function isAsAttachment():bool
+    {
+        return $this->asAttachment;
+    }
 }
